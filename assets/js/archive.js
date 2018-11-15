23185678634eeb15c0c1d6250593dd4eb4ab589f
srg1995 <sergio@iMac-de-sergio.local>
2018-11-16 00:54:58 +0100

    cambios capas info

--- assets/js/archive.js
+++ assets/js/archive.js
@@ -19,5 +19,24 @@ function menuTransparente(){
 }
 
 function efectoCapas(){
-    
+    $(".titulaciones").hover(function(){
+        console.log("elemento1")
+        $(".titulacion-elementos").toggle(500);
+    });
+
+    $(".experiencia").hover(function(){
+        console.log("elemento2")
+        $(".experiencia-elementos").toggle(500);
+    });
+
+    $(".conocimientos").hover(function(){
+        console.log("elemento3")
+        $(".conocimientos-elementos").toggle(500);
+    });
+
+    $(".mas-info").hover(function(){
+        console.log("elemento4")
+        $(".mas-info-elementos").toggle(500);
+    });
+
 }
